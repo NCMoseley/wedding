6a98e333541e6f25fc521aa065dda18eb0db258f
NCMoseley <ncmoseley@gmail.com>
2018-08-27 11:03:06 -0700

    Add boilerplate

---
+++ front-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package Streamline_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+
+      di
+
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
